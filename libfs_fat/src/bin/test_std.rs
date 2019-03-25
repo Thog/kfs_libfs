580f23c676a7645a2fab04af3dc2b16a2a1ad9cc
Thog <me@thog.eu>
2019-03-25 16:22:58 +0100

    test_std: clean up

--- libfs_fat/src/bin/test_std.rs
+++ libfs_fat/src/bin/test_std.rs
@@ -73,13 +73,12 @@ impl BlockDevice for LinuxBlockDevice {
     }
 }
 
-fn print_dir<T>(filesystem: &T, path: &str, level: u32, recursive: bool)
+fn print_dir<T>(filesystem: &T, path: &str, level: u32, recursive: bool) -> FileSystemResult<()>
 where
     T: FileSystemOperations,
 {
     let mut root_dir = filesystem
-        .open_directory(path, DirFilterFlags::ALL)
-        .unwrap();
+        .open_directory(path, DirFilterFlags::ALL)?;
 
     let mut entries: [DirectoryEntry; 1] = [DirectoryEntry {
         path: [0x0; DirectoryEntry::PATH_LEN],
@@ -105,54 +104,38 @@ where
             );
 
             if entry.entry_type == DirectoryEntryType::Directory && recursive {
-                print_dir(filesystem, entry_name, level + 1, recursive);
+                print_dir(filesystem, entry_name, level + 1, recursive)?;
             }
         }
     }
+
+    Ok(())
 }
 
-fn dump_to_file<'a>(file: &mut Box<dyn FileOperations + 'a>, path: &str) {
+fn dump_to_file<'a>(file: &mut Box<dyn FileOperations + 'a>, path: &str) -> FileSystemResult<()> {
     let mut f = File::create(path).unwrap();
 
-    let mut buffer: [u8; 0x1] = [0x0; 0x1];
+    let mut buffer: [u8; 0x200] = [0x0; 0x200];
     let mut offset = 0;
 
     loop {
-        let read_size = file.read(offset as u64, &mut buffer).unwrap() as usize;
-        trace!("{:x} = {:x}", offset, read_size);
+        let read_size = file.read(offset as u64, &mut buffer)? as usize;
         f.write_all(&buffer[0..read_size]).unwrap();
         if read_size == 0 {
             break;
         }
         offset += read_size;
     }
+
+    Ok(())
 }
 
-fn main() -> Result<()> {
+fn main() -> FileSystemResult<()> {
     env_logger::init();
 
-    let system_device = LinuxBlockDevice::new(std::env::args().nth(1).unwrap())?;
-    let filesystem = libfs_fat::FatFileSystem::get_raw_partition(system_device).unwrap();
-    /*filesystem.delete_file("/TEST/DUDE/HELLO.TXT").unwrap();
-    filesystem.delete_directory("/TEST/DUDE").unwrap();
-    filesystem.delete_directory("/TEST").unwrap();
-    filesystem.create_directory("/TEST").unwrap();
-    filesystem.create_directory("/TEST/DUDE").unwrap();
-    filesystem.create_file("/TEST/DUDE/HELLO.TXT", 42).unwrap();*/
-    //filesystem.delete_file("/hello_world.txt");
-    print_dir(&filesystem, "/", 0, false);
+    let system_device = LinuxBlockDevice::new(std::env::args().nth(1).unwrap()).unwrap();
+    let filesystem = libfs_fat::FatFileSystem::get_raw_partition(system_device)?;
 
-    let mut file = filesystem.open_file("/PRF2SAFE.RCV", FileModeFlags::READABLE).unwrap();
-    dump_to_file(&mut file, "PRF2SAFE.RCV");
-
-    //filesystem.rename_file("/PRF2SAFE.RCV", "/save/PRF2SAFE.RCV").unwrap();
-    /*filesystem
-        .rename_directory("/save", "/save_backup")
-        .unwrap();
-    print_dir(&filesystem, "/", 0, false);
-    print_dir(&filesystem, "/save_backup", 0, false);*/
-    //filesystem.create_file("/hello_world.txt", 42);
-    //print_dir(&filesystem, "/", 0, false);
-
+    print_dir(&filesystem, "/", 0, true)?;
     Ok(())
 }
