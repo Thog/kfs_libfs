784d52c9d78e4736d42872430a61272ea66d5585
Thog <tguillem@student.42.fr>
2019-03-28 17:05:37 +0100

    Start documenting libfs

--- libfs/src/block.rs
+++ libfs/src/block.rs
@@ -1,29 +1,47 @@
+/// Represent a block operation error.
 #[derive(Debug)]
-pub enum Error {
+pub enum BlockError {
+    /// Read error
+    ReadError,
+
+    /// Write error
+    WriteError,
+
+    /// Unknown error
     Unknown,
 }
 
-pub type Result<T> = core::result::Result<T, Error>;
+/// Represent a block operation result.
+pub type BlockResult<T> = core::result::Result<T, BlockError>;
 
+/// Represent a certain amount of data from a block device.
 #[derive(Clone)]
 pub struct Block {
+    /// The actual storage of the block.
     pub contents: [u8; Block::LEN],
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Represent the position of a block on a block device.
 pub struct BlockIndex(pub u32);
 
 #[derive(Debug, Copy, Clone)]
+/// Represent the count of blocks that a block device hold.
 pub struct BlockCount(pub u32);
 
 impl Block {
+    /// The size of a block in bytes.
     pub const LEN: usize = 512;
+
+    /// The size of a block in bytes as a 32 bits unsigned value.
     pub const LEN_U32: u32 = Self::LEN as u32;
 
+    /// Create a new block instance.
     pub fn new() -> Block {
-        Default::default()
+        Block::default()
     }
 
+    /// Return the content of the block.
     pub fn as_contents(&self) -> [u8; Block::LEN] {
         self.contents
     }
@@ -51,38 +69,48 @@ impl core::ops::DerefMut for Block {
 }
 
 impl BlockIndex {
+    /// Convert the block index into an offset in bytes.
     pub fn into_offset(self) -> u64 {
         u64::from(self.0) * (Block::LEN as u64)
     }
 }
 
 impl BlockCount {
+    /// Convert the block count into a size in bytes.
     pub fn into_size(self) -> u64 {
         u64::from(self.0) * (Block::LEN as u64)
     }
 }
 
+/// Represent a device holding blocks.
 pub trait BlockDevice: Sized {
-    fn raw_read(&self, blocks: &mut [Block], index: BlockIndex) -> Result<()>;
-    fn raw_write(&self, blocks: &[Block], index: BlockIndex) -> Result<()>;
 
+    /// Read blocks from the block device starting at the given ``index``.
+    fn raw_read(&self, blocks: &mut [Block], index: BlockIndex) -> BlockResult<()>;
+
+    /// Write blocks to the block device starting at the given ``index``.
+    fn raw_write(&self, blocks: &[Block], index: BlockIndex) -> BlockResult<()>;
+
+    /// Read blocks from the block device starting at the given ``partition_start + index``.
     fn read(
         &self,
         blocks: &mut [Block],
         partition_start: BlockIndex,
         index: BlockIndex,
-    ) -> Result<()> {
+    ) -> BlockResult<()> {
         self.raw_read(blocks, BlockIndex(partition_start.0 + index.0))
     }
 
+    /// Write blocks to the block device starting at the given ``partition_start + index``.
     fn write(
         &self,
         blocks: &[Block],
         partition_start: BlockIndex,
         index: BlockIndex,
-    ) -> Result<()> {
+    ) -> BlockResult<()> {
         self.raw_write(blocks, BlockIndex(partition_start.0 + index.0))
     }
 
-    fn count(&self) -> Result<BlockCount>;
+    /// Return the amount of blocks hold by the block device.
+    fn count(&self) -> BlockResult<BlockCount>;
 }
--- libfs/src/lib.rs
+++ libfs/src/lib.rs
@@ -1,89 +1,167 @@
+//! Interface to manipulate filesystem
 #![feature(alloc)]
 #![no_std]
+
+#![warn(clippy::cast_possible_wrap, clippy::cast_sign_loss, clippy::default_trait_access, clippy::explicit_into_iter_loop, clippy::explicit_iter_loop, clippy::missing_docs_in_private_items, clippy::mut_mut, clippy::replace_consts, clippy::used_underscore_binding, clippy::wildcard_dependencies, clippy::wrong_pub_self_convention)]
+
 
 extern crate alloc;
 
 #[macro_use]
 extern crate bitflags;
 
+/// I/O driver representation.
 pub mod block;
 
 use alloc::boxed::Box;
 
+/// Represent a filesystem error.
 #[derive(Debug)]
 pub enum FileSystemError {
+    /// The given resource couldn't be found.
     NotFound,
+
+    /// There isn't enough space for a resource to be stored.
     NoSpaceLeft,
+
+    /// The access to a given resource has been denied.
     AccessDenied,
+
+    /// A writing operation failed on the attached device block.
     WriteFailed,
+
+    /// A read operation failed on the attached device block.
     ReadFailed,
-    NoPartitionFound,
+
+    /// The given partition cannot be found.
     PartitionNotFound,
+
+    /// The given resource cannot be represented as a file.
     NotAFile,
+
+    /// The given resource cannot be represented as a directory.
     NotADirectory,
+
+    /// A resource at the given path already exist.
     FileExists,
+
+    /// The given path is too long to be resolved.
     PathTooLong,
-    UnknownPartitionFormat { partition_type: u32 },
+
+    /// The partition wasn't used as it's invalid.
     InvalidPartition,
-    Custom { name: &'static str },
+
+    /// Represent a custom error.
+    Custom {
+        /// The name of the custom error.
+        name: &'static str
+    },
 }
 
+/// Represent the type of a given resource when walking a directory.
 #[derive(Debug, PartialEq)]
 pub enum DirectoryEntryType {
+    /// The entry is a file.
     File,
+    /// The entry is a directory.
     Directory,
 }
 
+/// Represent an entry inside a directory.
 pub struct DirectoryEntry {
+    /// The path of the resource.
     pub path: [u8; Self::PATH_LEN],
+
+    /// The type of the resource.
     pub entry_type: DirectoryEntryType,
+
+    /// The size of the file. (0 if it's a directory)
     pub file_size: u64,
 }
 
 impl DirectoryEntry {
+    /// Represent the max path size (in bytes) supported.
     pub const PATH_LEN: usize = 0x301;
 }
 
 bitflags! {
+    /// Flags indicating the way a file should be open.
     pub struct FileModeFlags: u32 {
+        // The file should be readable.
         const READABLE = 0b0000_0001;
+
+        // The file should be writable.
         const WRITABLE = 0b0000_0010;
+
+        // The file should be appendable.
         const APPENDABLE = 0b0000_0100;
     }
 }
 
 bitflags! {
+    /// Flags indicating the filters when walking a directory.
     pub struct DirFilterFlags: u32 {
+        /// Accept directories.
         const DIRECTORY = 0b0000_0001;
+        
+        /// Accept files.
         const FILE = 0b0000_0010;
+        
+        /// Do not filter anything.
         const ALL = Self::DIRECTORY.bits | Self::FILE.bits;
     }
 }
 
+/// Represent the attached timestamp on a given resource.
 #[derive(Debug)]
 pub struct FileTimeStampRaw {
+    /// The resource creation UNIX timestamp.
     pub creation_timestamp: u64,
+
+    /// The resource last modification UNIX timestamp.
     pub modified_timestamp: u64,
+
+    /// The resource last access UNIX timestamp.
     pub accessed_timestamp: u64,
+
+    /// false if one of the given timestamp couldn't have been retrieved.
     pub is_valid: bool,
 }
 
+/// Represent a filesystem result.
 pub type FileSystemResult<T> = core::result::Result<T, FileSystemError>;
 
+/// Represent the operation on a file.
 pub trait FileOperations {
+    /// Read the content of a file at a given ``offset`` in ``buf``.
     fn read(&mut self, offset: u64, buf: &mut [u8]) -> FileSystemResult<u64>;
+
+    /// Write the content given ``buf`` at the given ``offset`` in the file.
+    /// If the file is too small to hold the data and the appendable flag is set, it will resize the file and append the data.
+    /// If the file is too small to hold the data and the appendable flag isn't set, this will return a FileSystemError::NoSpaceLeft.
     fn write(&mut self, offset: u64, buf: &[u8]) -> FileSystemResult<()>;
 
+    /// Flush any data not written on the filesystem.
     fn flush(&mut self) -> FileSystemResult<()>;
+
+    /// Resize the file with the given ``size``.
+    /// If the file isn't open with the appendable flag, it will not be extendable and will return a FileSystemError::NoSpaceLeft.
     fn set_len(&mut self, size: u64) -> FileSystemResult<()>;
+
+    /// Return the current file size.
     fn get_len(&mut self) -> FileSystemResult<u64>;
 }
 
+/// Represent the operation on a directory.
 pub trait DirectoryOperations {
+    /// Read the next directory entries and return the number of entries read.
     fn read(&mut self, buf: &mut [DirectoryEntry]) -> FileSystemResult<u64>;
+
+    /// Return the count of entries in the directory.
     fn entry_count(&self) -> FileSystemResult<u64>;
 }
 
+/// Represent the operation on a filesystem.
 pub trait FileSystemOperations {
     fn create_file(&self, path: &str, size: u64) -> FileSystemResult<()>;
     fn create_directory(&self, path: &str) -> FileSystemResult<()>;
